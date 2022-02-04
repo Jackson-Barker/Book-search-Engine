0563bb5c2d36db5b6453798876003dded048e9ac
jbarker <jackson.barker@du.edu>
2022-02-03 18:58:50 -0700

    queries.js

---
+++ Develop/client/src/utils/queries.js
@@ -0,0 +1,18 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+    me {
+        _id
+        username
+        email
+        bookCount
+        saveBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+`;
