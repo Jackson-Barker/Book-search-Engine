499e8c24453aafd31b11ab85e75e8b1900747d62
jbarker <jackson.barker@du.edu>
2022-02-03 17:30:52 -0700

    typedefs and index.js

--- Develop/server/models/Book.js
+++ Develop/server/models/Book.js
@@ -2,10 +2,6 @@ const { Schema } = require("mongoose");
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const bookSchema = new Schema({
-  bookID: {
-    type: String,
-    required: true,
-  },
   authors: [
     {
       type: String,
@@ -13,8 +9,10 @@ const bookSchema = new Schema({
   ],
   description: {
     type: String,
+    required: true,
   },
-  title: {
+  // saved book id from GoogleBooks
+  bookId: {
     type: String,
     required: true,
   },
@@ -24,34 +22,10 @@ const bookSchema = new Schema({
   link: {
     type: String,
   },
+  title: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = bookSchema;
-
-// const bookSchema = new Schema({
-
-//   // authors: [
-//   //   {
-//   //     type: String,
-//   //   },
-//   // ],
-//   // description: {
-//   //   type: String,
-//   //   required: true,
-//   // },
-//   // // saved book id from GoogleBooks
-//   // bookId: {
-//   //   type: String,
-//   //   required: true,
-//   // },
-//   // image: {
-//   //   type: String,
-//   // },
-//   // link: {
-//   //   type: String,
-//   // },
-//   // title: {
-//   //   type: String,
-//   //   required: true,
-//   // },
-// });
